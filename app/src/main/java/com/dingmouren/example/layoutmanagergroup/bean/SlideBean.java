1aa670bd8873f55e202622ce8ae7a96a3b830b3c
dingmouren <naildingmouren@gmail.com>
2018-05-08 19:52:57 +0800

    SlideLayoutManager竣工

---
+++ app/src/main/java/com/dingmouren/example/layoutmanagergroup/bean/SlideBean.java
@@ -0,0 +1,51 @@
+package com.dingmouren.example.layoutmanagergroup.bean;
+
+/**
+ * Created by dingmouren on 2018/5/8.
+ */
+
+public class SlideBean {
+    private int mItemBg;
+    private String mTitle;
+    private int mUserIcon;
+    private String mUserSay;
+
+    public SlideBean(int mItemBg, String mTitle, int mUserIcon, String mUserSay) {
+        this.mItemBg = mItemBg;
+        this.mTitle = mTitle;
+        this.mUserIcon = mUserIcon;
+        this.mUserSay = mUserSay;
+    }
+
+    public int getItemBg() {
+        return mItemBg;
+    }
+
+    public void setItemBg(int mItemBg) {
+        this.mItemBg = mItemBg;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public int getUserIcon() {
+        return mUserIcon;
+    }
+
+    public void setUserIcon(int mUserIcon) {
+        this.mUserIcon = mUserIcon;
+    }
+
+    public String getUserSay() {
+        return mUserSay;
+    }
+
+    public void setUserSay(String mUserSay) {
+        this.mUserSay = mUserSay;
+    }
+}
--- app/src/main/java/com/dingmouren/example/layoutmanagergroup/fragment/SlideFragment.java
+++ app/src/main/java/com/dingmouren/example/layoutmanagergroup/fragment/SlideFragment.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 
 import com.dingmouren.example.layoutmanagergroup.MyApplication;
 import com.dingmouren.example.layoutmanagergroup.R;
+import com.dingmouren.example.layoutmanagergroup.bean.SlideBean;
 import com.dingmouren.example.layoutmanagergroup.widget.SmileView;
 import com.dingmouren.layoutmanagergroup.slide.ItemConfig;
 import com.dingmouren.layoutmanagergroup.slide.ItemTouchHelperCallback;
@@ -38,25 +39,16 @@ public class SlideFragment extends Fragment {
     private ItemTouchHelper mItemTouchHelper;
     private ItemTouchHelperCallback mItemTouchHelperCallback;
     private MyAdapter mAdapter;
-    private static List<Integer> mImgList = new ArrayList<>();
+    private  List<SlideBean> mList = new ArrayList<>();
     private int mLikeCount = 50;
     private int mDislikeCount = 50;
-    private int mCurrentPosition = 50;
 
-    static {
-        mImgList.add(R.mipmap.img_slide_1);
-        mImgList.add(R.mipmap.img_slide_2);
-        mImgList.add(R.mipmap.img_slide_3);
-        mImgList.add(R.mipmap.img_slide_4);
-        mImgList.add(R.mipmap.img_slide_5);
-        mImgList.add(R.mipmap.img_slide_6);
-    }
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_slide,container,false);
+        View rootView = inflater.inflate(R.layout.fragment_slide, container, false);
         initView(rootView);
         initListener();
         return rootView;
@@ -71,77 +63,99 @@ public class SlideFragment extends Fragment {
 
         mAdapter = new MyAdapter();
         mRecyclerView.setAdapter(mAdapter);
-        mItemTouchHelperCallback = new ItemTouchHelperCallback(mRecyclerView.getAdapter(),mImgList);
+        addData();
+        mItemTouchHelperCallback = new ItemTouchHelperCallback(mRecyclerView.getAdapter(), mList);
         mItemTouchHelper = new ItemTouchHelper(mItemTouchHelperCallback);
-        mSlideLayoutManager = new SlideLayoutManager(mRecyclerView,mItemTouchHelper);
+        mSlideLayoutManager = new SlideLayoutManager(mRecyclerView, mItemTouchHelper);
         mRecyclerView.setLayoutManager(mSlideLayoutManager);
         mItemTouchHelper.attachToRecyclerView(mRecyclerView);
     }
 
-    private void initListener(){
+    private void initListener() {
         mItemTouchHelperCallback.setOnSlideListener(new OnSlideListener() {
             @Override
             public void onSliding(RecyclerView.ViewHolder viewHolder, float ratio, int direction) {
-                if (direction == ItemConfig.SLIDING_LEFT){
-                    Log.e(TAG,"onSling--左   radio:"+ratio+"  position:"+viewHolder.getLayoutPosition());
-                }else if (direction == ItemConfig.SLIDING_RIGHT){
-                    Log.e(TAG,"onSling--右   radio:"+ratio+"  position:"+viewHolder.getLayoutPosition());
+                if (direction == ItemConfig.SLIDING_LEFT) {
+                } else if (direction == ItemConfig.SLIDING_RIGHT) {
                 }
             }
 
             @Override
             public void onSlided(RecyclerView.ViewHolder viewHolder, Object o, int direction) {
-                if (direction == ItemConfig.SLIDED_LEFT){
+                if (direction == ItemConfig.SLIDED_LEFT) {
                     mDislikeCount--;
                     mSmileView.setDisLike(mDislikeCount);
                     mSmileView.disLikeAnimation();
-                }else if (direction == ItemConfig.SLIDED_RIGHT){
+                } else if (direction == ItemConfig.SLIDED_RIGHT) {
                     mLikeCount++;
                     mSmileView.setLike(mLikeCount);
                     mSmileView.likeAnimation();
                 }
-                mCurrentPosition--;
-                mAdapter.notifyItemChanged(0);
+                int position = viewHolder.getAdapterPosition();
+                Log.e(TAG, "onSlided--position:" + position);
             }
 
             @Override
             public void onClear() {
-
+                addData();
             }
         });
+    }
+
+    /**
+     * 向集合中添加数据
+     */
+    private void addData(){
+        int[] icons = {R.mipmap.header_icon_1, R.mipmap.header_icon_2, R.mipmap.header_icon_3,
+                R.mipmap.header_icon_4, R.mipmap.header_icon_1, R.mipmap.header_icon_2};
+        String[] titles = {"Acknowledging", "Belief", "Confidence", "Dreaming", "Happiness", "Confidence"};
+        String[] says = {
+                "Do one thing at a time, and do well.",
+                "Keep on going never give up.",
+                "Whatever is worth doing is worth doing well.",
+                "I can because i think i can.",
+                "Jack of all trades and master of none.",
+                "Keep on going never give up.",
+                "Whatever is worth doing is worth doing well.",
+        };
+        int[] bgs = {
+                R.mipmap.img_slide_1,
+                R.mipmap.img_slide_2,
+                R.mipmap.img_slide_3,
+                R.mipmap.img_slide_4,
+                R.mipmap.img_slide_5,
+                R.mipmap.img_slide_6
+        };
+
+        for (int i = 0; i < 6; i++) {
+            mList.add(new SlideBean(bgs[i],titles[i],icons[i],says[i]));
+        }
     }
 
 
     /**
      * 适配器
      */
-    class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
-        private int[] icons = {R.mipmap.header_icon_1,R.mipmap.header_icon_2,R.mipmap.header_icon_3,R.mipmap.header_icon_4};
-        private String[] titles = {"Acknowledging","Belief","Confidence","Dreaming","Happiness"};
-        private String[] says = {
-                "Do one thing at a time, and do well.",
-                "Keep on going never give up.",
-                "Whatever is worth doing is worth doing well.",
-                "I can because i think i can.",
-                "Jack of all trades and master of none."
-        };
+    class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
         @Override
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(MyApplication.sContext).inflate(R.layout.item_slide,parent,false);
+            View view = LayoutInflater.from(MyApplication.sContext).inflate(R.layout.item_slide, parent, false);
             return new ViewHolder(view);
         }
 
         @Override
         public void onBindViewHolder(ViewHolder holder, int position) {
-            holder.imgBg.setImageResource(mImgList.get(mCurrentPosition%6));
-            holder.userIcon.setImageResource(icons[mCurrentPosition%4]);
-            holder.tvTitle.setText(titles[mCurrentPosition%5]);
-            holder.userSay.setText(says[mCurrentPosition%5]);
+                SlideBean bean = mList.get(position);
+                holder.imgBg.setImageResource(bean.getItemBg());
+                holder.tvTitle.setText(bean.getTitle());
+                holder.userIcon.setImageResource(bean.getUserIcon());
+                holder.userSay.setText(bean.getUserSay());
         }
 
         @Override
         public int getItemCount() {
-            return 50;
+            return mList.size();
         }
 
         public class ViewHolder extends RecyclerView.ViewHolder {
@@ -149,6 +163,7 @@ public class SlideFragment extends Fragment {
             ImageView userIcon;
             TextView tvTitle;
             TextView userSay;
+
             public ViewHolder(View itemView) {
                 super(itemView);
                 imgBg = itemView.findViewById(R.id.img_bg);
