655206040c6a437a5da656427ff735d18c12e5de
dingmouren <1341520108@qq.com>
2018-05-09 17:22:09 +0800

    update

--- app/src/main/java/com/dingmouren/example/layoutmanagergroup/activity/SkidRightActivity_1.java
+++ app/src/main/java/com/dingmouren/example/layoutmanagergroup/activity/SkidRightActivity_1.java
@@ -2,17 +2,27 @@ package com.dingmouren.example.layoutmanagergroup.activity;
 
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.util.Pair;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.transition.Explode;
+import android.transition.Slide;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -31,13 +41,17 @@ public class SkidRightActivity_1 extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
     private SkidRightLayoutManager mSkidRightLayoutManager;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skid_1);
 
+
         initView();
     }
+
 
     private void initView() {
         mRecyclerView = findViewById(R.id.recycler_view);
@@ -50,7 +64,7 @@ public class SkidRightActivity_1 extends AppCompatActivity {
     /**
      * 适配器
      */
-    class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
+    class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         private int[] imgs = {
                 R.mipmap.skid_right_1,
                 R.mipmap.skid_right_2,
@@ -61,43 +75,44 @@ public class SkidRightActivity_1 extends AppCompatActivity {
                 R.mipmap.skid_right_7,
 
         };
-        String[] titles = {"Acknowledging", "Belief", "Confidence", "Dreaming", "Happiness", "Confidence"};
+        String[] titles = {"Acknowl", "Belief", "Confidence", "Dreaming", "Happiness", "Confidence"};
 
         @Override
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(MyApplication.sContext).inflate(R.layout.item_skid_right_1,parent,false);
+            View view = LayoutInflater.from(MyApplication.sContext).inflate(R.layout.item_skid_right_1, parent, false);
             return new ViewHolder(view);
         }
 
         @Override
         public void onBindViewHolder(final ViewHolder holder, final int position) {
-            Glide.with(MyApplication.sContext).load(imgs[position%7]).asGif().diskCacheStrategy(DiskCacheStrategy.SOURCE).into(holder.imgBg);
+            Glide.with(MyApplication.sContext).load(imgs[position % 7]).asGif().diskCacheStrategy(DiskCacheStrategy.SOURCE).into(holder.imgBg);
+            holder.tvTitle.setText(titles[position % 6]);
             holder.cardView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Intent intent = new Intent(SkidRightActivity_1.this, SkidRightActivity_2.class);
-                    intent.putExtra("img",imgs[position]);
-                    Pair<View, String> pair = new Pair<View, String>(holder.cardView,"card_view_1");
-//                    Pair<View,String> pairText = new Pair<View, String>(holder.text,"text");
-                    ActivityOptionsCompat optionsCompat =
-                            ActivityOptionsCompat.makeSceneTransitionAnimation(SkidRightActivity_1.this, pair);
-                    startActivity(intent,optionsCompat.toBundle());
+                    intent.putExtra("img", imgs[position % 7]);
+                    intent.putExtra("title", titles[position % 6]);
+                    startActivity(intent);
                 }
             });
         }
 
         @Override
         public int getItemCount() {
-            return 11;
+            return 20;
         }
 
         public class ViewHolder extends RecyclerView.ViewHolder {
             ImageView imgBg;
             CardView cardView;
+            TextView tvTitle;
+
             public ViewHolder(View itemView) {
                 super(itemView);
                 imgBg = itemView.findViewById(R.id.img_bg);
                 cardView = itemView.findViewById(R.id.card_view);
+                tvTitle = itemView.findViewById(R.id.tv_title);
             }
         }
     }
--- app/src/main/java/com/dingmouren/example/layoutmanagergroup/activity/SkidRightActivity_2.java
+++ app/src/main/java/com/dingmouren/example/layoutmanagergroup/activity/SkidRightActivity_2.java
@@ -3,8 +3,11 @@ package com.dingmouren.example.layoutmanagergroup.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -20,13 +23,19 @@ import com.dingmouren.example.layoutmanagergroup.R;
 
 public class SkidRightActivity_2 extends AppCompatActivity {
     private ImageView mImgBg;
+    private CardView mCardView;
+    private TextView mTvTitle;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skid_2);
         mImgBg = findViewById(R.id.img_bg);
+        mCardView = findViewById(R.id.card_view);
+        mTvTitle = findViewById(R.id.tv_title);
         if (getIntent() != null){
             int imgPath = getIntent().getIntExtra("img",R.mipmap.skid_right_3);
+            String title = getIntent().getStringExtra("title");
+            mTvTitle.setText(title);
             Glide.with(MyApplication.sContext).load(imgPath).asGif().diskCacheStrategy(DiskCacheStrategy.SOURCE).into(mImgBg);
         }
     }
